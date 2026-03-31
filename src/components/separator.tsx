63acf445c2b6943f04eb694f5f3b10532728eca8
Im-younique <dyyim4725@gmail.com>
2026-03-31 23:35:21 +0900

    fix: Tabs/Separator data-horizontal 속성 불일치로 레이아웃 깨짐 수정
    base-ui가 data-orientation="horizontal"을 설정하지만
    Tailwind의 data-horizontal: variant는 boolean [data-horizontal] 속성을 기대.
    orientation에 따라 data-horizontal/data-vertical boolean 속성을 명시적으로 추가.

--- src/components/separator.tsx
+++ src/components/separator.tsx
@@ -13,6 +13,7 @@ function Separator({
     <SeparatorPrimitive
       data-slot="separator"
       orientation={orientation}
+      {...(orientation === "horizontal" ? { "data-horizontal": "" } : { "data-vertical": "" })}
       className={cn(
         "shrink-0 bg-border data-horizontal:h-px data-horizontal:w-full data-vertical:w-px data-vertical:self-stretch",
         className
